62016535e48e0c8f8cbbf3155fbb49f7da5e7862
jermainehunter <jermainehunter@gmail.com>
2019-04-05 22:54:28 -0700

    fixed config file for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else {
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,6 +13,7 @@ const connection = mysql.createConnection({
   password: "password",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(err => {
